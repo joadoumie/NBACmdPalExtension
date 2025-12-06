cdd3758a3050575f72633928cc08a573e325d0eb
Jordi Adoumie <98557455+joadoumie@users.noreply.github.com>
2025-12-05 19:07:01 -0500

    view source

--- NBAExtension/NBAExtensionCommandsProvider.cs
+++ NBAExtension/NBAExtensionCommandsProvider.cs
@@ -17,6 +17,7 @@ public partial class NBAExtensionCommandsProvider : CommandProvider
         return [
             new CommandContextItem(new OpenUrlCommand("https://github.com/joadoumie/NBACmdPalExtension/issues/new") { Name = "Request New Feature", Result = CommandResult.Dismiss() }) { Icon = new IconInfo("\uD83C\uDFC0") },
             new CommandContextItem(new OpenUrlCommand("https://github.com/joadoumie/NBACmdPalExtension/issues/new") { Name = "Report a Bug", Result = CommandResult.Dismiss() }) { Icon = new IconInfo("\uD83D\uDC1B") },
+            new CommandContextItem(new OpenUrlCommand("https://github.com/joadoumie/NBACmdPalExtension") { Name = "View Source Code", Result = CommandResult.Dismiss() }) { Icon = new IconInfo("\u2328\uFE0F") },
         ];
     }
 
@@ -25,7 +26,7 @@ public partial class NBAExtensionCommandsProvider : CommandProvider
         DisplayName = "View Upcoming NBA Games";
         Icon = IconHelpers.FromRelativePath("Assets\\StoreLogo.png");
         _commands = [
-            new CommandItem(new ViewGamesDynamicPage()) { Title = DisplayName, MoreCommands = GetAboutContextItems() },
+            new CommandItem(new ViewGamesDynamicPage()) { Title = DisplayName, Icon = new IconInfo("https://a.espncdn.com/combiner/i?img=/i/teamlogos/leagues/500/nba.png&w=64&h=64&transparent=true"), MoreCommands = GetAboutContextItems() },
             new CommandItem(new SampleGalleryListPage()) { Title = "Sample Gallery List Page" },
         ];
     }
